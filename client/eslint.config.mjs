043c2c0d83275ef688bf58f16aa71b9f6094c4a0
Arthur <hlushko.arthur@gmail.com>
2025-03-12 11:57:37 +0200

    updated eslint

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -4,15 +4,16 @@ import tseslint from "typescript-eslint";
 import angular from "angular-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+export default tseslint.config([
 	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
-	...angular.configs.tsRecommended,
-	...tseslint.configs.stylistic,
-	...tseslint.configs.strict,
 	{
 		files: ["**/*.ts"],
+		extends: [
+			pluginJs.configs.recommended,
+			...angular.configs.tsRecommended,
+			...tseslint.configs.stylistic,
+			...tseslint.configs.strict,
+		],
 		rules: {
 			"@angular-eslint/component-selector": [
 				"error",
@@ -64,6 +65,88 @@ export default [
 				}
 			],
 			"lines-between-class-members": "error",
+			"semi": ["error", "always"],
 		},
+	},
+	{
+		files: ["**/*.html"],
+		extends: [
+			...angular.configs.templateRecommended,
+			...angular.configs.templateAccessibility
+		],
+		rules: {}
 	}
-];
+]);
+
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+// import angular from "angular-eslint";
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+// 	{ languageOptions: { globals: globals.browser } },
+// 	pluginJs.configs.recommended,
+// 	...angular.configs.tsRecommended,
+// 	...tseslint.configs.stylistic,
+// 	...tseslint.configs.strict,
+// 	{
+// 		files: ["**/*.ts"],
+// 		rules: {
+// 			"@angular-eslint/component-selector": [
+// 				"error",
+// 				{
+// 					type: "element",
+// 					prefix: "",
+// 					style: "kebab-case",
+// 				},
+// 			],
+// 			"no-console": ["warn", {
+// 				allow: ["error", "warn"],
+// 			}],
+// 			"@typescript-eslint/naming-convention": [
+// 				"error",
+// 				{
+// 					"selector": ["function", "method"],
+// 					"format": ["strictCamelCase"],
+// 					"leadingUnderscore": "allow"
+// 				},
+// 				{
+// 					"selector": ["property", "variable"],
+// 					"format": ["camelCase", "UPPER_CASE"],
+// 					"leadingUnderscore": "allow"
+// 				}
+// 			],
+// 			"@typescript-eslint/explicit-function-return-type": "error",
+// 			"@typescript-eslint/no-unused-vars": "error",
+// 			"keyword-spacing": ["error", { "before": true, "after": true }],
+// 			"indent": ["error", "tab"],
+// 			"padding-line-between-statements": [
+// 				"error",
+// 				{
+// 					"blankLine": "always",
+// 					"prev": "*",
+// 					"next": "*"
+// 				},
+// 				{
+// 					"blankLine": "never",
+// 					"prev": "import",
+// 					"next": "import"
+// 				},
+// 				{
+// 					"blankLine": "never",
+// 					"prev": [
+// 						"case",
+// 						"default"
+// 					],
+// 					"next": "*"
+// 				}
+// 			],
+// 			"lines-between-class-members": "error",
+// 		},
+// 	},
+// 	{
+// 		files: ["**/*.html"],
+// 		rules: {}
+// 	}
+// ];
--- client/src/app/pages/guest/games/games.component.ts
+++ client/src/app/pages/guest/games/games.component.ts
@@ -22,6 +22,7 @@ export class GamesComponent implements OnInit {
 
 		if (!steamId) {
 			this._router.navigateByUrl('/');
+
 			return;
 		}
 
